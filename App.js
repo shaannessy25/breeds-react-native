44ef44b1a80a7e05512805fb4fa8acea30ad6354
shaannessy25 <shaan.hurley25@gmail.com>
2020-11-09 11:00:33 -0800

    list of dogs displays on screen

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Items from './items'
+import { cats, dogs } from './breeds'
+
+
 
 export default function App() {
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.h1}>hello</Text>
+      <FlatList
+        style={styles.list}
+        data={dogs}
+        renderItem={({index, item}) => {
+          return <Items name={item.breed} />
+        }}
+      />
     </View>
   );
 }
@@ -16,6 +28,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  h1: {
+    color: 'red',
+    fontSize: 50,
+  },
+  list: {
+    flex: 1,
+    width: "100%"
+  }
 });
---
+++ items.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+
+const Items = ({name}) => {
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.breed}>{name}</Text>
+    </View>
+  )
+}
+
+export default Items
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    width: "100%"
+  },
+  breed: {
+    fontSize: 24
+  }
+})
