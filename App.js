fe783b6f9d13629139e7b56c93d4fcca1ae372a1
shaannessy25 <shaan.hurley25@gmail.com>
2020-11-11 13:34:00 -0800

    adding search feature

--- App.js
+++ App.js
@@ -1,24 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, SafeAreaView, TextInput, KeyboardAvoidingView } from 'react-native';
 import Items from './items'
 import { cats, dogs } from './breeds'
 import Properties from './itemProperty'
 
 
 export default function App() {
-
+  const [ query, setQuery ] = useState('')
   return (
 
     <SafeAreaView style={styles.container}>
-      <Text style={styles.h1}>Breed App</Text>
-      <FlatList
-        style={styles.list}
-        data={cats}
-        renderItem={({index, item}) => {
-          return <Properties item={item} />
-        }}
-      />
+      <KeyboardAvoidingView
+        behavior={Platform.OS == "ios" ? "padding" : "height"}
+        style={styles.container}
+      >
+
+        <Text style={styles.h1}>Breed App</Text>
+        <FlatList
+          style={styles.list}
+          data={cats.filter((item) => item.breed.toLowerCase().includes(query.toLowerCase()))}
+          renderItem={({index, item}) => {
+            return <Properties item={item} />
+          }}
+        />
+
+        <TextInput 
+          style={styles.input}
+          onChangeText={text => setQuery(text)}
+          value={query}
+        />
+        <StatusBar style="auto" />
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
@@ -29,6 +42,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'flex-start',
+    width: '100%',
   },
   h1: {
     color: 'red',
@@ -37,5 +51,10 @@ const styles = StyleSheet.create({
   list: {
     flex: 1,
     width: "100%"
-  }
+  },
+  input: {
+    width: '100%', 
+    borderWidth: 1,
+    padding: 10
+  },
 });
